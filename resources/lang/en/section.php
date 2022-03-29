3d73c53c4e7deef3187c43cad9bc99c7211fff27
Mostafa Moradi <mostafa90fx@gmail.com>
2022-03-29 17:18:46 +0300

    theme setting section added, help links added in theme setting and located in topbar

--- resources/lang/en/section.php
+++ resources/lang/en/section.php
@@ -4,4 +4,16 @@ return [
     'theme_settings' => [
         'name' => 'Theme Settings'
     ],
+    'general_setting' => [
+        'name' => 'Theme Settings'
+    ],
+    'help' => [
+        'name' => 'Help'
+    ],
+    'dark_mode' => [
+        'name' => 'Dark Mode'
+    ],
+    'light_mode' => [
+        'name' => 'Light Mode'
+    ]
 ];

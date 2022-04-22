f452cb0229971e3902f91beda1895a73123701e1
Mostafa Moradi <mostafa21tr@gmail.com>
2022-04-22 18:45:58 +0300

    paddle js

--- resources/js/paddle.js
+++
@@ -1,8 +0,0 @@
-Paddle.Setup({ vendor: 4062 });
-function openCheckout(id, email = null) {
-    Paddle.Checkout.open({
-        product: id,
-        email: email
-    });
-}
-$('.buy').on('click', (e) => openCheckout($(e.target).data('id')));

4f843da7efd4185df91435a6092dee1cff0de477
spektra2147 <spektra2147@gmail.com>
2022-05-19 12:43:20 +0300

    update sidebar

--- resources/js/ocify-sidebar.js
+++ resources/js/ocify-sidebar.js
@@ -5,22 +5,23 @@ $("#menu-toggle").click(function(e) {
      $("#menu-toggle-2").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled-2");
+        $(".top-bar").toggleClass("wide-top-bar");
         $("#main").toggleClass("wide-content");
     });
 
-     function initMenu() {
-      $('#ocify-menu li a').click(
-        function() {
-          var checkElement = $(this).next();
-          if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
-            return false;
-            }
-          if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
-            $('#ocify-menu ul:visible').slideUp('normal');
-            checkElement.slideDown('normal');
-            return false;
-            }
-          }
-        );
-      }
-    $(document).ready(function() {initMenu();});
+    //  function initMenu() {
+    //   $('#ocify-menu li a').click(
+    //     function() {
+    //       var checkElement = $(this).next();
+    //       if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+    //         return false;
+    //         }
+    //       if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+    //         $('#ocify-menu ul:visible').slideUp('normal');
+    //         checkElement.slideDown('normal');
+    //         return false;
+    //         }
+    //       }
+    //     );
+    //   }
+    // $(document).ready(function() {initMenu();});
